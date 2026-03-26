64ab0c911d572fae17b4df912336595f73ddd734
Tristan Labram <tristan.labram@live.co.uk>
2026-03-26 19:52:59 +0000

    Addition of first user model migrations, following successful access of the Django administration page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# User manegement UI Components (Built on Djangos UserAdmin pre-built pages)
+class CustomUserAdmin(UserAdmin):
+    # Use our custom user model to use email for login and set roles
+    model = CustomUser
+
+    # User list page
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active')
+
+    # Default sort order (trailing ',' for python tuple)
+    ordering = ('email',)
+
+    # EDIT forms for existing users
+    # (<Section Name>, {'fields': (<modelProperty1>, <modelProperty2>)})
+    fieldsets = (
+        # Login form (not sure how this is an edit form but that's Django apparently)
+        (None, {
+            "fields": (
+                'email',
+                'password'
+            ),
+        }),
+        # Personal info form
+        ('Personal Info', {
+            "fields": (
+                'first_name',
+                'last_name',
+                'role'
+            ),
+        }),
+        # Permissions form
+        ('Permissions', {
+            "fields": (
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ),
+        }),
+    )
+
+    # CREATE forms for new users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'first_name',
+                'last_name',
+                'role',
+                'password1',
+                'password2',
+            ),
+        }),
+    )
+    
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+# User repository functions
+class CustomUserManager(BaseUserManager):
+    # Create a new user
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    # Create a new admin user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        return self.create_user(email, password, **extra_fields)
+
+# User model
+class CustomUser(AbstractUser):
+    # Role choices enum (trailing ',' for python tuple notation)
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('viewer', 'View-Only'),
+    ]
+    # Remove username, using emails for auth
+    username = None
+
+    # Define required fields
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    # Define custom access roles
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='viewer')
+
+    # Attach custom manager to get additional fields
+    objects = CustomUserManager()
+
+    # Use email for login
+    USERNAME_FIELD = 'email'
+    # Additional required fields (above Django defaults: email, password etc)
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # ToString() Equivalent
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role})"
+    
+    # IsAdmin Getter 
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
